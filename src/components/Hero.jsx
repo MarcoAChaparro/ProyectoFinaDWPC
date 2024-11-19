5577dce0796b0961c5e0a843432a72928f863d8a
marco antonio chaparro morales <marco.chaparro@uao.edu.co>
2024-11-19 15:32:11 -0500

    imagen

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,17 +1,13 @@
 import './Hero.css';
-import Background from './imagenes/Background.webp';
-import Ensalada from './imagenes/ensalada.jpg';
-import Quinoa from './imagenes/quinoa.webp';
 
 
 const Hero = () => {
     return (
         <div className="hero">
             <div className="slider">
-                <img src={Background} alt="Imagen 1" />
-                <img src={Ensalada} alt="Imagen 2" />
-<               img src={Quinoa} alt="Imagen 3" />
-
+                <img src="/imagenes/Background.webp" alt="Imagen 1" />
+                <img src="/imagenes/ensalada.jpg" alt="Imagen 2" />
+                <img src="/imagenes/quinoa.webp" alt="Imagen 3" />
             </div>
             <div className="hero-content">
                 <h1>Bienvenido a Fitness Bites</h1>
